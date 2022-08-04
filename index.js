06fff65d67fae6a46150f69b6c9e9f37a2d69e85
Jack <jackdonnell74@gmail.com>
2022-08-05 03:37:27 +0700

    15

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-
-
 const baseURL = `http://localhost:9876`
 
 const getMay = document.querySelector("#may-budget")
 const getJune = document.querySelector("#june-budget")
 const getJuly = document.querySelector("#july-budget")
 
-const summaryContainer = document.querySelector("#summary-container")
-
 const expenseAndCostSubmit = document.querySelector("#income-tax-submit")
 const nameAndIncomeAndTaxSubmit = document.querySelector("#name-income-tax-submit")
+
+const budgetContainer = document.querySelector("#budget-container")
+const summaryContainer = document.querySelector("#summary-container")
+
 const getMayBudget = () => {
     axios.get(`${baseURL}/api/getMay`)
         .then((res) => {
@@ -36,7 +36,6 @@ const taxInput = document.querySelector("#tax")
         Name: nameInput.value,
         Income: incomeInput.value,
         Tax: taxInput.value
-
     }
     axios.post(`${baseURL}/api/postNameAndIncomeAndTax`, budgetDatabaseObj) 
         .then((res) => {
@@ -58,27 +57,36 @@ const newExpense = () => {
     })
 }
 
-// const summary = () => {
-//     const summaryDiv = document.createElement('div')
-//     summaryDiv.classList.add('summary')
+let summaryArr = [monthlyInc, sum, incomeRemaining]
 
-//     summaryDiv.innerHTML = `<p>Your monthly income after tax is ${monthlyInc}</p><p>You've spent ${sum}</p><p>You have ${incomeRemaining} remaining</p></div>`
+const summary = (arr) => {
+    const summaryDiv = document.createElement('div')
+    summaryDiv.classList.add('summary')
+
+    summaryDiv.innerHTML = `<p>Your monthly income after tax is ${arr[0]}</p><p>You've spent ${arr[1]}</p><p>You have ${arr[2]} remaining</p></div>`
 
 
-//     summaryContainer.appendChild(summaryDiv)
-// }
+    summaryContainer.appendChild(summaryDiv)
+}
 
-// function displayMovies(arr) {
-//     moviesContainer.innerHTML = ``
+// function displaySumary() {
+//     budgetContainer.innerHTML = ``
 //     for (let i = 0; i < arr.length; i++) {
-//         createMovieCard(arr[i])
+//         create(arr[i])
 //     }
 // }
 
 // form.addEventListener('submit', submitHandler)
 
 // getAllMovies()
-nameAndIncomeAndTaxSubmit.addEventListener("click", postNameAndIncomeAndTax)
+
+const runEverything = () => {
+summary(summaryArr)
+postNameAndIncomeAndTax(2)
+
+}
+
+nameAndIncomeAndTaxSubmit.addEventListener("click", runEverything)
 expenseAndCostSubmit.addEventListener("click", newExpense)
 
 
